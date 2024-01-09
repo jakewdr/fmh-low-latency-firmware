78a0db936f1d39285685d582cd01b4d86820b03e
jake <C92i747A@pm.me>
2024-01-09 15:39:34 +0000

    Added files

---
+++ config.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2022 Gondolindrim
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* I don't recommend changing these too much */
+
+#pragma once
+
+#define I2C_SCL_PIN B6
+#define I2C_SDA_PIN B7
+#define I2C_DRIVER I2CD1
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2
+
+#define I2C_SCL_PAL_MODE 4
+#define I2C_SDA_PAL_MODE 4
+#define I2C1_CLOCK_SPEED 1000000 /* Default is 4khz (this is 10khz) */
+#define EEPROM_I2C_24LC256
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
+
+/* My settings */
+
+#define LAYER_STATE_8BIT
+
+#define DEBOUNCE 3
+#define MATRIX_IO_DELAY 20
+#define QMK_KEYS_PER_SCAN 10
+#define USB_POLLING_INTERVAL_MS 1
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+/* 
+
+Some information for why I chose the 1mhz clock speed
+
+~ http://tinyurl.com/stm32mhzkhz
+
+*/
---
+++ rules.mk
@@ -0,0 +1,25 @@
+# Build Options ->
+# TODO explicitly disable more shiz
+
+NKRO_ENABLE = yes          # Enable N-Key Rollover
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite bootloader (so useful!)
+LTO_ENABLE = yes		   # Enables link time optimizations for small compile size
+
+MOUSEKEY_ENABLE = no       # Mouse keys
+CONSOLE_ENABLE = no        # Console for debug
+EXTRAKEY_ENABLE = no       # Audio control and System control
+
+AUDIO_ENABLE = no          # Audio output
+ENCODER_ENABLE = no		   # Enables rotary encoders
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+SPLIT_KEYBOARD = no		   # Enables split keyboard options
+COMMAND_ENABLE = no        # Commands for debug and configuration
+BACKLIGHT_ENABLE = no      # Enables keyboard backlight functionality
+
+MIDI_ENABLE = no # Fuck no
+BLUETOOTH_ENABLE = no # Also fuck no
+RING_BUFFERED_6KRO_REPORT_ENABLE = no # I prefer NKRO
+COMMAND_ENABLE = no # Debugging isn't needed when the keyboard works
+MOUSE_SHARED_EP = no # The digital mouse doesn't need to share a pin with NKRO when it is disabled
+
+DEBOUNCE_TYPE = sym_eager_pk # Around a 5ms decrease in latency over the default
